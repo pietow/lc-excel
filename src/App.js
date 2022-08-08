3ef0a5e0f047aa9a28be8fd7d8c91a53db6a7dd2
piet <pprotassow@posteo.de>
2022-08-08 18:11:45 +0200

    list with input field

--- src/App.js
+++ src/App.js
@@ -7,25 +7,34 @@ function App() {
     const [data, setData] = useState(iniData)
     const [edit, setEdit] = useState(null)
 
+    //if cell is double clicked, set edit to the cell's index
     function showEditor(e) {
         console.log(e.target.dataset.id)
         setEdit({
-            row: parseInt(e.target.parentNode.dataset.row),
+            row: parseInt(e.target.dataset.id),
         })
     }
 
+    //save cell input to data
     function save(e) {
         e.preventDefault()
         const input = e.target.firstChild
         const dataCopy = [...data]
-        dataCopy[edit.row][0] = input.value
-        setEdit(null)
+        dataCopy[edit.row] = input.value
+        setEdit(null) //reset edit to null, therefore no input field is generated
         setData(dataCopy)
     }
 
     return (
         <ul onDoubleClick={showEditor}>
             {data.map((item, index) => {
+                if (edit && edit.row === index) {
+                    item = (
+                        <form onSubmit={save}>
+                            <input type="text" defaultValue={item} />
+                        </form>
+                    )
+                }
                 return (
                     <li data-id={index} key={index}>
                         {item}
