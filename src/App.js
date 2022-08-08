3bcc73c97cb86060eb3dbbd248b918a3df1aedfe
piet <pprotassow@posteo.de>
2022-08-08 17:59:18 +0200

    data-id --> dataset.id

--- src/App.js
+++ src/App.js
@@ -5,16 +5,34 @@ import './style.css'
 
 function App() {
     const [data, setData] = useState(iniData)
+    const [edit, setEdit] = useState(null)
+
+    function showEditor(e) {
+        console.log(e.target.dataset.id)
+        setEdit({
+            row: parseInt(e.target.parentNode.dataset.row),
+        })
+    }
+
+    function save(e) {
+        e.preventDefault()
+        const input = e.target.firstChild
+        const dataCopy = [...data]
+        dataCopy[edit.row][0] = input.value
+        setEdit(null)
+        setData(dataCopy)
+    }
+
     return (
-        <table>
-            <tbody>
-                {data.map((row, idx) => (
-                    <tr key={idx}>
-                        <td>{row[0]}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <ul onDoubleClick={showEditor}>
+            {data.map((item, index) => {
+                return (
+                    <li data-id={index} key={index}>
+                        {item}
+                    </li>
+                )
+            })}
+        </ul>
     )
 }
 
